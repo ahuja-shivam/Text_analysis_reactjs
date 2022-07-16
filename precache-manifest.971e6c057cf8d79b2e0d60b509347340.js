dfddff018b9431a643e2ca155a0f25f514840db6
Shivam Ahuja <ahujas1203@gmail.com>
2022-07-16 19:07:38 +0530

    Updates

---
+++ precache-manifest.971e6c057cf8d79b2e0d60b509347340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "746c3a071bfe3453b25e",
+    "url": "/Text_analysis_reactjs/static/css/main.65c32436.chunk.css"
+  },
+  {
+    "revision": "746c3a071bfe3453b25e",
+    "url": "/Text_analysis_reactjs/static/js/main.746c3a07.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Text_analysis_reactjs/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "95f3db8699b1a942c513",
+    "url": "/Text_analysis_reactjs/static/js/2.95f3db86.chunk.js"
+  },
+  {
+    "revision": "a1acd2bb5e885f6bf1a6",
+    "url": "/Text_analysis_reactjs/static/js/runtime~main.a1acd2bb.js"
+  },
+  {
+    "revision": "d28f618f835d93a6d229ffe8f9e2c632",
+    "url": "/Text_analysis_reactjs/index.html"
+  }
+];
